b3e5554640fc20f4688445e74912304d6c2e3199
Ryoschin <nikolagagic5127@gmail.com>
2024-03-12 12:11:11 +0100

    New commit

--- 02.cpp
+++ 02.cpp
@@ -8,83 +8,60 @@ int main() {
     string line;
     vector<string> matrix;
     while (getline(cin, line)) {
-        if (line != "") {
-            matrix.push_back(line);
-        } else {
-            break;
-        }
+        matrix.push_back(line);
     }
 
     int n = matrix.size();
     int m = matrix[0].size();
-    vector<vector<int>> grid(n, vector<int>(m, -1));
+    // grid[i][j] = 0 - zaglavljena
+    // grid[i][j] = 1 - izlazi
+    // grid[i][j] = 2 - ne znamo
+    // grid[i][j] = 3 - zid
+    vector<int> grid(m, 0);
 
-    for (int j = m - 1; j >= 0; j--) {
-        if (matrix[n - 1][j] == '.') {
-            grid[n - 1][j] = 1;
-        } else {
-            grid[n - 1][j] = 3;
-        }
+    for (int j = 0; j < m; j++) {
+        grid[j] = (matrix[n - 1][j] == '.') ? 1 : 3;
     }
 
     for (int i = n - 2; i >= 0; i--) {
-        // for (int k = 0; k < n; k++) {
-        //     for (int l = 0; l < m; l++) {
-        //         cout << grid[k][l];
-        //     }
-        //     cout << endl;
-        // }
-        // cout << endl;
-        for (int j = m - 1; j >= 0; j--) {
+        for (int j = 0; j < m; j++) {
             if (matrix[i][j] == '.') {
-                if (grid[i + 1][j] == 1) {
-                    grid[i][j] = 1;
-                } else {
-                    if (grid[i + 1][j] == 3) {
-                        if (j < m - 1 && grid[i][j + 1] == 1)
-                            grid[i][j] = 1;
-                        else
-                            grid[i][j] = 2;
-                    } else {
-                        grid[i][j] = 0;
-                    }
+                switch (grid[j]) {
+                    int k;
+                    case 1:
+                        grid[j] = 1;
+                        k = j - 1;
+                        while (k >= 0 && grid[k] == 2) {
+                            grid[k] = 1;
+                            k--;
+                        }
+                        break;
+                    case 3:
+                        grid[j] = (j > 0 && grid[j - 1] == 1) ? 1 : 2;
+                        break;
+                    default:
+                        grid[j] = 0;
+                        k = j - 1;
+                        while (k >= 0 && grid[k] == 2) {
+                            grid[k] = 0;
+                            k--;
+                        }
                 }
             } else {
-                grid[i][j] = 3;
-            }
-
-            if ((grid[i][j] != 2 && j < m - 1) || j == 0) {
-                int k = j + 1, num = 0;
-                if (j == 0 && grid[i][j] == 2) {
-                    grid[i][j] = 0;
-                }
-                if (grid[i][j] == 1) {
-                    num = 1;
-                } else {
-                    num = 0;
-                }
-                while (grid[i][k] == 2) {
-                    grid[i][k] = num;
-                    k++;
+                grid[j] = 3;
+                int k = j - 1;
+                while (k >= 0 && grid[k] == 2) {
+                    grid[k] = 0;
+                    k--;
                 }
             }
         }
     }
-
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < m; j++) {
-    //         cout << grid[i][j];
-    //     }
-    //     cout << endl;
-    // }
 
     for (int j = 0; j < m; j++) {
-        if (grid[0][j] == 3) {
-            cout << 0;
-        } else {
-            cout << grid[0][j];
-        }
+        cout << ((grid[j] == 1) ? 1 : 0);
     }
 
+    cout << endl;
     return 0;
 }

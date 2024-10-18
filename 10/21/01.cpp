73f9cd0eed5e26c74a225cc43ec72948bed8cbde
Ryoschin <nikolagagic5127@gmail.com>
2024-10-19 01:32:33 +0200

    19/10/2024/01:32:32

--- 10/21/01.cpp
+++ 10/21/01.cpp
@@ -2,7 +2,124 @@
 
 using namespace std;
 
+const float defaultZapremina = 100;
+const float defaultStanje = 0;
+
+class Rezervoar {
+   private:
+    float zapremina;
+    float stanje;
+
+   public:
+    Rezervoar() {
+        this->stanje = defaultStanje;
+        this->zapremina = defaultZapremina;
+    }
+
+    Rezervoar(float zapremina) {
+        this->setZapremina(zapremina);
+        this->stanje = defaultStanje;
+    }
+
+    Rezervoar(float zapremina, float stanje) {
+        this->setZapremina(zapremina);
+        this->setStanje(stanje);
+    }
+
+    void setZapremina(float zapremina) {
+        this->zapremina = (zapremina <= 0) ? defaultZapremina : zapremina;
+    }
+
+    void setStanje(float stanje) {
+        if (stanje < 0) {
+            this->stanje = defaultStanje;
+        } else if (stanje > this->zapremina) {
+            this->stanje = this->zapremina;
+        } else {
+            this->stanje = stanje;
+        }
+    }
+
+    float getZapremina() const {
+        return this->zapremina;
+    }
+
+    float getStanje() const {
+        return this->stanje;
+    }
+
+    float dopuni(float kolicina) {
+        float stanje = this->stanje;
+        float novoStanje = this->stanje + kolicina;
+
+        // Ako je kolicina < 0? Ovde ću pretpostaviti da je input korektan jer zadatak ne definiše taj slučaj
+        this->stanje = (novoStanje > this->zapremina) ? this->zapremina : novoStanje;
+        return this->stanje - stanje;
+    }
+
+    float istoci(float kolicina) {
+        float stanje = this->stanje;
+        float novoStanje = this->stanje - kolicina;
+
+        // Ako je kolicina < 0? Ovde ću pretpostaviti da je input korektan jer zadatak ne definiše taj slučaj
+        this->stanje = (novoStanje < 0) ? 0 : novoStanje;
+        return stanje - this->stanje;
+    }
+
+    float tankuj() {
+        float kolicina = this->zapremina - this->stanje;
+        this->stanje = this->zapremina;
+        return kolicina;
+    }
+
+    float isprazni() {
+        float kolicina = this->stanje - 0;
+        this->stanje = 0;
+        return kolicina;
+    }
+
+    float kapacitetPunjenja() {
+        return this->zapremina - this->stanje;
+    }
+};
+
+void printRezervoar(Rezervoar rezervoar) {
+    cout << rezervoar.getStanje() << " / " << rezervoar.getZapremina() << ", " << rezervoar.kapacitetPunjenja() << endl;
+}
+
 int main() {
-    cout << "Hello, world!" << endl;
+    Rezervoar rezervoar = Rezervoar();
+    printRezervoar(rezervoar);  // 0 / 100, 100
+
+    rezervoar.setStanje(60);
+    cout << rezervoar.dopuni(30) << " " << rezervoar.getStanje() << endl;  // 30 90
+
+    rezervoar.setStanje(60);
+    cout << rezervoar.dopuni(50) << " " << rezervoar.getStanje() << endl;  // 40 100
+
+    rezervoar = Rezervoar(-123);
+    printRezervoar(rezervoar);  // 0 / 100, 100
+
+    rezervoar.setStanje(30);
+    cout << rezervoar.istoci(20) << " " << rezervoar.getStanje() << endl;  // 20 10
+
+    rezervoar.setStanje(30);
+    cout << rezervoar.istoci(50) << " " << rezervoar.getStanje() << endl;  // 30 0
+
+    rezervoar = Rezervoar(1, -123);
+    printRezervoar(rezervoar);  // 0 / 1, 1
+
+    rezervoar.setZapremina(100);
+    rezervoar.setStanje(30);
+    cout << rezervoar.tankuj() << " " << rezervoar.getStanje() << endl;  // 70 100
+
+    rezervoar = Rezervoar(1, 2);
+    printRezervoar(rezervoar);  // 1 / 1, 0
+
+    rezervoar.setZapremina(100);
+    rezervoar.setStanje(30);
+    cout << rezervoar.kapacitetPunjenja() << endl;                         // 70
+    cout << rezervoar.isprazni() << " " << rezervoar.getStanje() << endl;  // 30 0
+
     return 0;
 }
---
+++ 10/21/02.cpp
@@ -0,0 +1,164 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+enum paket {
+    CLASSIC,
+    GOLD,
+    VIP
+};
+
+const paket defaultVrstaRacuna = CLASSIC;
+const float defaultStanje = 0;
+
+class Racun {
+   private:
+    string vlasnikIme;
+    string vlasnikPrezime;
+    paket vrstaRacuna;
+    float stanje;
+
+   public:
+    Racun(string vlasnikIme, string vlasnikPrezime) {
+        this->setVlasnikIme(vlasnikIme);
+        this->setVlasnikPrezime(vlasnikPrezime);
+        this->vrstaRacuna = defaultVrstaRacuna;
+        this->stanje = defaultStanje;
+    }
+
+    Racun(string vlasnikIme, string vlasnikPrezime, paket vrstaRacuna) {
+        this->setVlasnikIme(vlasnikIme);
+        this->setVlasnikPrezime(vlasnikPrezime);
+        this->setVrstaRacuna(vrstaRacuna);
+        this->stanje = defaultStanje;
+    }
+
+    Racun(string vlasnikIme, string vlasnikPrezime, paket vrstaRacuna, float stanje) {
+        this->setVlasnikIme(vlasnikIme);
+        this->setVlasnikPrezime(vlasnikPrezime);
+        this->setVrstaRacuna(vrstaRacuna);
+        this->setStanje(stanje);
+    }
+
+    void setVlasnikIme(string vlasnikIme) {
+        size_t start = vlasnikIme.find_first_not_of(' ');
+        if (start == std::string::npos) {
+            this->vlasnikIme = "";
+        } else {
+            size_t end = vlasnikIme.find_last_not_of(' ');
+            this->vlasnikIme = vlasnikIme.substr(start, end - start + 1);
+        }
+
+        if (this->vlasnikIme.length() > 20) this->vlasnikIme = this->vlasnikIme.substr(0, 20);
+    }
+
+    void setVlasnikPrezime(string vlasnikPrezime) {
+        size_t start = vlasnikPrezime.find_first_not_of(' ');
+        if (start == std::string::npos) {
+            this->vlasnikPrezime = "";
+        } else {
+            size_t end = vlasnikPrezime.find_last_not_of(' ');
+            this->vlasnikPrezime = vlasnikPrezime.substr(start, end - start + 1);
+        }
+
+        if (this->vlasnikPrezime.length() > 30) this->vlasnikPrezime = this->vlasnikPrezime.substr(0, 30);
+    }
+
+    void setVrstaRacuna(paket vrstaRacuna) {
+        this->vrstaRacuna = vrstaRacuna;
+    }
+
+    void setStanje(float stanje) {
+        this->stanje = (stanje < 0) ? defaultStanje : stanje;
+    }
+
+    string getVlasnikIme() const {
+        return this->vlasnikIme;
+    }
+
+    string getVlasnikPrezime() const {
+        return this->vlasnikPrezime;
+    }
+
+    paket getVrstaRacuna() const {
+        return this->vrstaRacuna;
+    }
+
+    float getStanje() const {
+        return this->stanje;
+    }
+
+    void uplata(float suma) {
+        if (suma > 0) this->stanje += suma;
+    }
+
+    void isplata(float suma) {
+        if (suma >= this->stanje) {
+            switch (this->vrstaRacuna) {
+                case CLASSIC:
+                    this->stanje -= this->stanje * 0.9;
+                    break;
+
+                case GOLD:
+                    this->stanje -= this->stanje * 0.95;
+                    break;
+
+                case VIP:
+                    this->stanje = 0;
+            }
+        } else {
+            this->stanje -= suma;
+        }
+    }
+
+    void stampa() {
+        string vrstaRacuna;
+        switch (this->vrstaRacuna) {
+            case CLASSIC:
+                vrstaRacuna = "CLASSIC";
+                break;
+
+            case GOLD:
+                vrstaRacuna = "GOLD";
+                break;
+
+            case VIP:
+                vrstaRacuna = "VIP";
+        }
+        cout << this->vlasnikIme << " " << this->vlasnikPrezime << " - " << vrstaRacuna << " - " << this->stanje << endl;
+    }
+};
+
+int main() {
+    Racun racun = Racun("   PetarPetarPetarPetarPetarPetarPetarPetarPetarPetarPetar                                                                              ", "                                                                             PetrovicPetrovicPetrovicPetrovicPetrovicPetrovicPetrovicPetrovic      ");
+    racun.uplata(123);
+    racun.stampa();  // PetarPetarPetarPetar PetrovicPetrovicPetrovicPetrov - CLASSIC - 123
+
+    racun = Racun("Petar", "Petrovic", CLASSIC);
+    racun.uplata(-123);
+    racun.stampa();  // Petar Petrovic - CLASSIC - 0
+
+    racun = Racun("Petar", "Petrovic", GOLD, 123);
+    racun.isplata(23);
+    racun.stampa();  // Petar Petrovic - GOLD - 100
+
+    racun = Racun("Petar", "Petrovic", VIP, -123);
+    cout << racun.getVlasnikIme() << " " << racun.getVlasnikPrezime() << " - " << racun.getVrstaRacuna() << " - " << racun.getStanje() << endl;  // Petar Petrovic - 2 - 0
+
+    racun.setVrstaRacuna(CLASSIC);
+    racun.setStanje(10000);
+    racun.isplata(11000);
+    cout << racun.getStanje() << endl;  // 1000
+
+    racun.setVrstaRacuna(GOLD);
+    racun.setStanje(10000);
+    racun.isplata(11000);
+    cout << racun.getStanje() << endl;  // 500
+
+    racun.setVrstaRacuna(VIP);
+    racun.setStanje(10000);
+    racun.isplata(11000);
+    cout << racun.getStanje() << endl;  // 0
+
+    return 0;
+}
